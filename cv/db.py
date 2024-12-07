41dcc4e034ab8b8fe6ef44ffb377c6b0dad5fa48
graham <grahamf56@pm.me>
2024-12-07 03:40:37 +0000

    1.0.3 connection string to Dockerfile

--- cv/db.py
+++ cv/db.py
@@ -1,12 +1,15 @@
+import os
 from azure.cosmosdb.table import TableService, Entity
-the_connection_string = ""
+the_connection_string = os.environ.get['AZURE_COSMOS_CONNECTION_STRING']
+if not the_connection_string:
+    raise ValueError("AZURE_COSMOS_CONNECTION_STRING environment variable is not set")
+
 table_service = TableService(endpoint_suffix="table.cosmos.azure.com", connection_string=the_connection_string)
 
 table_name = 'comments'
 table_service.create_table(table_name)
 
 def add_comment(name, email, comment):
-    
     entity = Entity()
     entity.PartitionKey = 'comments'
     entity.RowKey = f"{name}_{email}"
